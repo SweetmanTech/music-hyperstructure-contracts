6c30342d33f4ebd700552808525ff4b2e6c5014a
Sweets Sweetman <sweetmantech@gmail.com>
2022-08-05 15:06:22 -0300

    I init CatalogFactory contract.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,39 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+require("@openzeppelin/hardhat-upgrades");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.13",
+  solidity: {
+    version: "0.8.15",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {},
+    polygonMumbai: {
+      url: process.env.MUMBAI_RPC,
+      accounts: [process.env.TESTNET_PRIVATE_KEY],
+    },
+    mainnet: {
+      url: "https://rpc.ankr.com/eth",
+      accounts: [process.env.TESTNET_PRIVATE_KEY],
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_RPC,
+      accounts: [process.env.TESTNET_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY,
+      goerli: process.env.ETHERSCAN_API_KEY,
+      rinkeby: process.env.ETHERSCAN_API_KEY,
+      mainnet: process.env.ETHERSCAN_API_KEY,
+    },
+  },
 };
